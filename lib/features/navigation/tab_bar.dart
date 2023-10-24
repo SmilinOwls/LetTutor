8bafe972b7e2d4408d0a0f886dd4e7af8a70eb27
20120234 <20120234@student.hcmus.edu.vn>
2023-10-24 16:47:36 +0700

    refactor: use  class to store the tab list including entries with label and icon

--- lib/features/navigation/tab_bar.dart
+++ lib/features/navigation/tab_bar.dart
@@ -13,39 +13,40 @@ class _TabBarNavigatorState extends State<TabBarNavigator> {
   List<Widget> screens = [
     const HomeScreen(),
   ];
-  final List<String> _tabList = ['Home', 'Tutor', 'Schedule', 'Courses', 'Settings'];
+
+  final Map<String, IconData> _tabList = {
+    'Home': Icons.home,
+    'Tutor': Icons.people,
+    'Schedule': Icons.schedule_outlined,
+    'Courses': Icons.school,
+    'Setting': Icons.settings,
+  };
 
   int _activeTab = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: CustomAppBar(textLeading: _tabList[_activeTab]),
+      appBar: CustomAppBar(textLeading: _tabList.keys.elementAt(_activeTab)),
       body: screens[_activeTab],
       bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        selectedLabelStyle: TextStyle(
-          fontSize: 16,
-          color: Theme.of(context).primaryColor,
-        ),
-        selectedItemColor: Theme.of(context).primaryColor,
-        onTap: (value) {
-          setState(() {
-            _activeTab = value;
-          });
-        },
-        elevation: 18,
-        currentIndex: _activeTab,
-        items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-          BottomNavigationBarItem(icon: Icon(Icons.people), label: 'Tutors'),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.schedule_outlined), label: 'Schedule'),
-          BottomNavigationBarItem(icon: Icon(Icons.school), label: 'Courses'),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.settings), label: 'Settings'),
-        ],
-      ),
+          type: BottomNavigationBarType.fixed,
+          selectedLabelStyle: TextStyle(
+            fontSize: 16,
+            color: Theme.of(context).primaryColor,
+          ),
+          selectedItemColor: Theme.of(context).primaryColor,
+          onTap: (value) {
+            setState(() {
+              _activeTab = value;
+            });
+          },
+          elevation: 18,
+          currentIndex: _activeTab,
+          items: _tabList.entries
+              .map<BottomNavigationBarItem>((MapEntry entry) =>
+                  BottomNavigationBarItem(label: entry.key, icon: Icon(entry.value)))
+              .toList()),
     );
   }
 }
