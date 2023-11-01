ef33a536986f7c855216c204d4d7c53a942987aa
20120234 <20120234@student.hcmus.edu.vn>
2023-11-02 03:09:15 +0700

    style: add tutor rating dialog for history screen

---
+++ lib/features/history/widgets/history_rating_dialog.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:lettutor/widgets/lesson_dialog.dart';
+import 'package:lettutor/widgets/star_rating.dart';
+
+class HistoryRatingDialog extends StatefulWidget {
+  const HistoryRatingDialog({super.key});
+
+  @override
+  State<HistoryRatingDialog> createState() => _HistoryRatingDialogState();
+}
+
+class _HistoryRatingDialogState extends State<HistoryRatingDialog> {
+  final TextEditingController _reviewTextEditingController =
+      TextEditingController();
+  double _rating = 5;
+
+  @override
+  void dispose() {
+    super.dispose();
+    _reviewTextEditingController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return LessonDialog(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            'What is your rating for Keegan?',
+            style: Theme.of(context).textTheme.bodyLarge,
+          ),
+          const SizedBox(height: 14),
+          StarRating(
+            rating: _rating,
+            onRatingChanged: (value) => setState(() {
+              _rating = value;
+            }),
+          ),
+          const SizedBox(height: 20),
+          SizedBox(
+            height: 120,
+            child: TextField(
+              maxLines: null,
+              expands: true,
+              controller: _reviewTextEditingController,
+              keyboardType: TextInputType.multiline,
+              onChanged: (value) {},
+              style: const TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.normal,
+              ),
+              decoration: const InputDecoration(
+                isCollapsed: true,
+                contentPadding: EdgeInsets.all(12),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    width: 0.5,
+                    color: Colors.grey,
+                  ),
+                ),
+                hintText: 'Content Review',
+                hintStyle: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w400,
+                  color: Colors.grey,
+                ),
+                border: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    width: 0.5,
+                    color: Colors.grey,
+                  ),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    width: 1,
+                    color: Colors.blue,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+      onSubmit: () {
+        return null;
+      },
+    );
+  }
+}
--- lib/widgets/star_rating.dart
+++ lib/widgets/star_rating.dart
@@ -1,11 +1,39 @@
 import 'package:flutter/material.dart';
 
 class StarRating extends StatelessWidget {
-  const StarRating({super.key, required this.rating});
+  const StarRating({super.key, required this.rating, this.onRatingChanged});
 
   final double rating;
-  
+  final void Function(double)? onRatingChanged;
+
+  String getRatingToolTipMessage(int index)  {
+    String message;
+
+    switch (index) {
+      case 0:
+        message = 'terrible';
+        break;
+      case 1:
+        message = 'bad';
+        break;
+      case 2:
+        message = 'normal';
+        break;
+      case 3:
+        message = 'good';
+        break;
+      case 4:
+        message = 'wonderfull';
+        break;
+      default:
+        message = 'wonderfull';
+    }
+
+    return message;
+  }
+
   Widget buildStar(BuildContext context, int index) {
+
     Icon icon;
     if (index >= rating) {
       icon = Icon(
@@ -21,13 +49,32 @@ class StarRating extends StatelessWidget {
       );
     }
     return InkResponse(
-      onTap: (){},
-      child: icon,
+      onTap: onRatingChanged == null ? null : () => onRatingChanged!(index + 1),
+      onHover: (value) async {
+        if (value && onRatingChanged != null) {
+          onRatingChanged!(index + 1);
+        }
+      },
+      child: onRatingChanged == null
+          ? icon
+          : Tooltip(
+              message: getRatingToolTipMessage(index),
+              waitDuration: const Duration(milliseconds: 200),
+              child: icon,
+            ),
     );
   }
 
   @override
   Widget build(BuildContext context) {
-    return Row(children: List<Widget>.generate(5, (index) => buildStar(context, index)));
+    return Row(
+      mainAxisAlignment: onRatingChanged == null
+          ? MainAxisAlignment.start
+          : MainAxisAlignment.center,
+      children: List<Widget>.generate(
+        5,
+        (index) => buildStar(context, index),
+      ),
+    );
   }
 }
